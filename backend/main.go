a269cb1bec13ac78688863eeaa31a8083de8b5ce
Ajeszny <245107@edu.p.lodz.pl>
2025-01-13 16:52:11 +0100

    Added main

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"time"
+)
 
+func get_pastas(r http.ResponseWriter, q *http.Request) {
+	r.WriteHeader(200)
+	r.Write([]byte("[{\"id\": 1, \"name\":\"Вован\", \"tags\": []}]"))
+}
+
+func main() {
+	http.HandleFunc("/get_pasta_list", get_pastas)
+	s := &http.Server{
+		Addr:           ":8000",
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	err := s.ListenAndServe()
+	print(err)
 }
